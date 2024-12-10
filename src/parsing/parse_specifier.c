8af5be9815639354ea9957fe21b860ace5d1642a
ratchetandcunt <42insidethebox@gmail.com>
2024-12-10 16:58:21 +0100

    printf

--- src/parsing/parse_specifier.c
+++ src/parsing/parse_specifier.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parse_specifier.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: procha-r <procha-r@student.42.fr>          +#+  +:+       +#+        */
+/*   By: pedroribeiro <pedroribeiro@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 15:55:45 by pedroribeir       #+#    #+#             */
-/*   Updated: 2024/11/17 16:54:56 by procha-r         ###   ########.fr       */
+/*   Updated: 2024/12/08 23:07:03 by pedroribeir      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,6 @@ void	parse_specifier(const char **format, t_format *fmt)
 	char	spec;
 
 	spec = **format;
-	if (**format == 'l')
-	{
-		fmt->length_modifier = 'l';
-		(*format)++;
-	}
 	if (spec == 'c' || spec == 's' || spec == 'd' || \
 		spec == 'i' || spec == 'u' || spec == 'x' || \
 		spec == 'X' || spec == 'p' || spec == '%')
